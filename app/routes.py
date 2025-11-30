d2bd6811d4e3df1ceb4f16b2a3202bb8d38e3171
Arielle Ezechukwu <a.ezechuk@gmail.com>
2025-11-29 21:21:59 -0600

    Update templates and routes file

--- app/routes.py
+++ app/routes.py
@@ -5,4 +5,15 @@ from app import app
 @app.route('/index')
 def index():
     user = {'username': 'Arielle'}
-    return render_template('index.html', title='Home', user=user)
+    posts = [
+        {
+            'author': {'username': 'Camille'}, 'body': 'Basketball for Beginners!'
+        },  
+        {
+            'author': {'username': 'Zaid'}, 'body': 'Spelling Bee Prep Tips!'
+        },
+        {
+            'author': {'username': 'Pokey'}, 'body': 'The Best foods for Bulking Up!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
