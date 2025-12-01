c967d74561714c98ab8678738d7b9c1ace70bbc8
Arielle Ezechukwu <a.ezechuk@gmail.com>
2025-12-01 13:16:34 -0600

    Add user registration, login, logout, and form validation

--- app/models.py
+++ app/models.py
@@ -1,10 +1,13 @@
+from app import login
+from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime, timezone
 from typing import Optional
 import sqlalchemy as sa
 import sqlalchemy.orm as so
 from app import db
+from flask_login import UserMixin
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
     # always present, integer primary key
     username: so.Mapped[str] = so.mapped_column(sa.String(64), index=True, unique=True)
@@ -18,6 +21,14 @@ class User(db.Model):
     def __repr__(self):
         return f'<User {self.username}>'
     
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+    # generate and store the password hash for the user
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+    # Return True if the given password matches the stored hash
+
 class Post(db.Model): 
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
     body: so.Mapped[str] = so.mapped_column(sa.String(140))
@@ -32,3 +43,7 @@ class Post(db.Model):
 
     def __repr__(self):
         return f'<Post {self.body}>'
+    
+@login.user_loader
+def load_user(id):
+    return db.session.get(User, int(id))
