a4a08cbcf0c5fea9f38d0ab34160cfc466f4d5a4
Arielle Ezechukwu <a.ezechuk@gmail.com>
2025-12-01 14:54:25 -0600

    Add ability to edit profile, include about me and last seen

--- app/models.py
+++ app/models.py
@@ -1,11 +1,11 @@
-from app import login
+from app import login, db
 from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime, timezone
 from typing import Optional
 import sqlalchemy as sa
 import sqlalchemy.orm as so
-from app import db
 from flask_login import UserMixin
+from hashlib import md5
 
 class User(UserMixin, db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
@@ -17,6 +17,11 @@ class User(UserMixin, db.Model):
     password_hash: so.Mapped[Optional[str]] = so.mapped_column(sa.String(256))
     # not required can be none (before user sets password)
     posts: so.WriteOnlyMapped['Post'] = so.relationship(back_populates='author')
+    about_me: so.Mapped[Optional[str]] = so.mapped_column(sa.String(140))
+    last_seen: so.Mapped[Optional[datetime]] = so.mapped_column(
+        default=lambda: datetime.now(timezone.utc))
+
+    
     
     def __repr__(self):
         return f'<User {self.username}>'
@@ -28,6 +33,10 @@ class User(UserMixin, db.Model):
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
     # Return True if the given password matches the stored hash
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return f'https://www.gravatar.com/avatar/{digest}?d=identicon&s={size}'
+
 
 class Post(db.Model): 
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
