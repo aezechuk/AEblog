e30d5ac7882f83893fb6afc2b58300f1cefbdaa2
Arielle Ezechukwu <a.ezechuk@gmail.com>
2025-12-04 15:14:26 -0600

    pagination

--- app/routes.py
+++ app/routes.py
@@ -25,8 +25,16 @@ def index():
         flash('Your post is now live!')
         return redirect(url_for('index'))
     
-    posts = db.session.scalars(current_user.following_posts()).all()
-    return render_template('index.html', title='Home', form=form, posts=posts)
+    page = request.args.get('page', 1, type=int)
+    posts = db.paginate(current_user.following_posts(), page=page,
+                        per_page=app.config['POSTS_PER_PAGE'], error_out=False)
+    next_url = url_for('index', page=posts.next_num) \
+        if posts.has_next else None
+    prev_url = url_for('index', page=posts.prev_num) \
+        if posts.has_prev else None
+    return render_template('index.html', title='Home', form=form,
+                           posts=posts.items, next_url=next_url,
+                           prev_url=prev_url)
 
 @app.route('/user/<username>')
 @login_required
@@ -35,14 +43,18 @@ def user(username):
         sa.select(User).where(User.username == username))
     if user is None:
         abort(404)
-
-    posts = db.session.scalars(
-        sa.select(Post).where(Post.user_id == user.id)
-        .order_by(Post.timestamp.desc())
-        ).all()
-    
+    page = request.args.get('page', 1, type=int)
+    query = sa.select(Post).where(Post.user_id == user.id).order_by(Post.timestamp.desc())
+    posts = db.paginate(query, page=page,
+                        per_page=app.config['POSTS_PER_PAGE'],
+                        error_out=False)
+    next_url = url_for('user', username=user.username, page=posts.next_num) \
+        if posts.has_next else None
+    prev_url = url_for('user', username=user.username, page=posts.prev_num) \
+        if posts.has_prev else None
     form = EmptyForm()
-    return render_template('user.html', user=user, posts=posts, form=form)
+    return render_template('user.html', user=user, posts=posts.items,
+                           next_url=next_url, prev_url=prev_url, form=form)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -142,6 +154,13 @@ def unfollow(username):
 @app.route('/explore')
 @login_required
 def explore():
+    page = request.args.get('page', 1, type=int)
     query = sa.select(Post).order_by(Post.timestamp.desc())
-    posts = db.session.scalars(query).all()
-    return render_template('index.html', title='Explore', posts=posts, form=None)
+    posts = db.paginate(query, page=page,
+                        per_page=app.config['POSTS_PER_PAGE'], error_out=False)
+    next_url = url_for('explore', page=posts.next_num) \
+        if posts.has_next else None
+    prev_url = url_for('explore', page=posts.prev_num) \
+        if posts.has_prev else None
+    return render_template('index.html', title='Explore', posts=posts.items,
+                           next_url=next_url, prev_url=prev_url, form=None)
