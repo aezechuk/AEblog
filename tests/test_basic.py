4d8a44fe86dae7b14fd97123b011e37f05552e27
Arielle Ezechukwu <a.ezechuk@gmail.com>
2025-12-11 10:46:51 -0600

    Add basic test for login page

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,3 +1,8 @@
 def test_home_page_loads(client):
     response = client.get("/")
     assert response.status_code == 200
+
+def test_login_page_loads(client):
+    response = client.get("/login")
+    assert response.status_code == 200
+    
