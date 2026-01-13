2e921d8feed09270589b632d6d19a2c020e599f5
Arielle Ezechukwu <a.ezechuk@gmail.com>
2026-01-13 11:10:01 -0600

    update conftest to app env variable at top

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,7 @@
-import sys
 import os
+os.environ["DATABASE_URL"] = "sqlite:///:memory:"
 
+import sys
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 import pytest
