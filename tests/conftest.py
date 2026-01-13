6930f2e0a99fae2926c7c853cb0e6bd47446b8f3
Arielle Ezechukwu <a.ezechuk@gmail.com>
2026-01-13 11:21:53 -0600

    replace conftest with temporary workaround until refactory complete

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,27 +1,30 @@
 import os
-os.environ["DATABASE_URL"] = "sqlite:///:memory:"
-
 import sys
+import tempfile
+import pytest
+
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
-import pytest
 from app import app, db
 
 @pytest.fixture
 def client():
-    app.config.update(TESTING = True,
-                      SQLALCHEMY_DATABASE_URI = "sqlite:///:memory:",
-                      SQLALCHEMY_BINDS={}, 
-                      WTF_CSRF_ENABLED = False)
+    # Create temp database file
+    db_fd, db_path = tempfile.mkstemp()
 
-    
+    app.config.update(
+        TESTING=True,
+        SQLALCHEMY_DATABASE_URI=f"sqlite:///{db_path}",
+        WTF_CSRF_ENABLED=False,
+    )
+
     with app.app_context():
-        db.engines.clear()  # forces old connection to drop and create new one
         db.create_all()
-        
+
         with app.test_client() as client:
             yield client
 
         db.drop_all()
 
-# Enables in-memory db, flask-login compatibility, isolated tests
+    os.close(db_fd)
+    os.unlink(db_path)
