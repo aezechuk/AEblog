ee17d6eff2c23059a2f064901444fc4252c910a8
Arielle Ezechukwu <a.ezechuk@gmail.com>
2026-01-16 13:17:45 -0600

    move email file to auth folder and updated auth route

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -4,7 +4,7 @@ from app.auth.forms import LoginForm, RegistrationForm, ResetPasswordRequestForm
 from app import db
 import sqlalchemy as sa
 from app.models import User
-from app.email import send_password_reset_email
+from app.auth.email import send_password_reset_email
 from urllib.parse import urlsplit
 
 
