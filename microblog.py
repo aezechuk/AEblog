a69fd820258a51412f15605d4235a43b7a8a6585
Arielle Ezechukwu <arielleezechukwu@Arielles-MacBook-Air.local>
2026-01-19 14:38:02 -0600

    added lines to directly open app

--- microblog.py
+++ microblog.py
@@ -8,3 +8,6 @@ app = create_app()
 @app.shell_context_processor
 def make_shell_context():
     return {'sa': sa, 'so': so, 'db': db, 'User': User, 'Post': Post}
+
+if __name__ == "__main__":
+    app.run()
