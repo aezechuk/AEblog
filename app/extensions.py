1d23d22edd6fc9a8a7b79054637496a7dd237ea7
Arielle Ezechukwu <arielleezechukwu@Arielles-MacBook-Air.local>
2026-01-19 22:32:23 -0600

    Initialize CSRF protection

--- app/extensions.py
+++ app/extensions.py
@@ -3,6 +3,7 @@ from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_mail import Mail
 from flask_moment import Moment
+from flask_wtf import CSRFProtect
 
 
 db = SQLAlchemy()
@@ -10,3 +11,4 @@ migrate = Migrate()
 login = LoginManager()
 mail = Mail()
 moment = Moment()
+csrf = CSRFProtect()
