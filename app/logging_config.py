ffd3533a10bf91d8363d5447ca7ec96d5f294a9c
Arielle Ezechukwu <a.ezechuk@gmail.com>
2026-01-26 19:27:30 -0600

    added configure_logging function to use watchtower for audit logging

---
+++ app/logging_config.py
@@ -0,0 +1,14 @@
+import watchtower, logging
+
+def configure_logging(app):
+    logger = logging.getLogger("security.audit")
+    logger.setLevel(logging.INFO)
+    
+    if not logger.handlers:
+        handler = watchtower.CloudWatchLogHandler(log_group_name="aeblog/app")
+        logger.addHandler(handler)
+    
+    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(name)s - %(message)s')
+    handler.setFormatter(formatter)
+
+    logger.info("security logger initialized")
